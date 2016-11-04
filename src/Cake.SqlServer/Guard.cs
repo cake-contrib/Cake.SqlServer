ae7223889a2fcfb37aa99fa2eef9df1980f8313a
trailmax <trailmax1@gmail.com>
2016-11-04 17:42:36 +0000

    Guard class and refactoring for it

---
+++ src/Cake.SqlServer/Guard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
+namespace Cake.SqlServer
+{
+    internal static class Guard
+    {
+        [DebuggerHidden]
+        internal static void ArgumentIsNotNull(object value, string argumentName)
+        {
+            if (value is String)
+            {
+                var stringValue = value as String;
+                if (String.IsNullOrWhiteSpace(stringValue))
+                {
+                    throw new ArgumentNullException(argumentName);
+                }
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
---
+++ src/Tests/GuardTests.cs
@@ -0,0 +1,36 @@
+﻿using Cake.SqlServer;
+using NUnit.Framework;
+using System;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class GuardTests
+    {
+        [TestCase((String)null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+        [TestCase("\r\n")]
+        public void String_Empty_ThrowsException(String value)
+        {
+            // Act
+            Action act = () => Guard.ArgumentIsNotNull(value, "value");
+
+            // Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void NullObject_Throws_Exception()
+        {
+            //Arrange
+            object value = null;
+
+            Action act = () => Guard.ArgumentIsNotNull(value, "value");
+
+            // Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
